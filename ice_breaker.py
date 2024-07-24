80e4d1912e2ef9606521fe735eafaee02bfa74f3
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2024-07-24 12:33:54 +0100

    format fix

--- ice_breaker.py
+++ ice_breaker.py
@@ -10,7 +10,9 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
     summary_template = """
             given the Linkedin information {information} about a person I want you to create:
             1. a short summary
@@ -18,7 +20,7 @@ def ice_break_with(name: str) -> str:
         """
 
     summary_prompt_template = PromptTemplate(
-            input_variables=["information"], template=summary_template
+        input_variables=["information"], template=summary_template
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
@@ -34,4 +36,3 @@ if __name__ == "__main__":
     load_dotenv()
     print("Ice Breaker Enter ")
     ice_break_with(name="Eden Marco Udemy")
-
--- tools/tools.py
+++ tools/tools.py
@@ -4,5 +4,5 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 def get_profile_url_tavily(name: str) -> str:
     """Searches for LinkedIn or Twitter Profile Page."""
     search = TavilySearchResults()
-    res = search.run(f'{name}')
+    res = search.run(f"{name}")
     return res[0]["url"]
