cc4a44ebcb12c82daea43691fa9c75f31acb1ebb
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2024-07-26 09:26:00 +0100

    added, implemented outout parser

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,36 +1,44 @@
+import os
 from dotenv import load_dotenv
-from langchain.prompts.prompt import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
-from langchain.chains import LLMChain
-
+from langchain_community.chat_models import ChatOllama
+from output_parsers import summary_parser
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
-
+    linkedin_data = scrape_linkedin_profile(
+        linkedin_profile_url=linkedin_username, mock=True
+    )
     summary_template = """
-    given the Linkedin information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
+            given the Linkedin information {information} about a person I want you to create:
+            1. a short summary
+            2. two interesting facts about them
+            
+            \n{format_instructions}
+        """
+
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables=["information"],
+        template=summary_template,
+        partial_variables={"format_instructions": summary_parser.get_format_instructions()}
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
-    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    # chain = summary_prompt_template | llm | StrOutputParser()
+    chain = summary_prompt_template | llm | summary_parser
 
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+    return res
 
 
 if __name__ == "__main__":
     load_dotenv()
-
-    print("Ice Breaker Enter")
-    ice_break_with(name="Eden Marco")
+    print("Ice Breaker Enter ")
+    ice_break_with(name="Eden Marco Udemy")
